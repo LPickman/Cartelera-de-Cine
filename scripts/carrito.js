e41f49da10b9eafd4f8126e7494e82264b2673b9
LPickman <pickmanleandro1@gmail.com>
2024-07-03 19:37:57 -0300

    Se arregló la falla del sidebar. No estaba cambiado a right los canvas. Por otro lado, se agregó un loading a las busquedas

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -3,85 +3,78 @@ function mostrarValorSeleccionado() {
   const valorSeleccionado = select.options[select.selectedIndex].text;
   const precio = document.getElementById("precio");
   const total = document.getElementById("precio-total");
-  const asientos = document.getElementById("asientos");
   const cantidadEntradas = document.getElementById("cantidad-entradas").value;
-  let precio2D = 9200;
-  let precio3D = 11200;
+  const precio2D = 9200;
+  const precio3D = 11200;
 
   if (valorSeleccionado.includes("2D")) {
     precio.innerHTML = `$${precio2D}`;
-    if (cantidadEntradas == 1) {
-      total.innerHTML = `${cantidadEntradas} Entrada: $${
-        cantidadEntradas * precio2D
-      }`;
-    } else {
-      total.innerHTML = `${cantidadEntradas} Entradas: $${
-        cantidadEntradas * precio2D
-      }`;
-    }
+    total.innerHTML = `${cantidadEntradas} Entrada${
+      cantidadEntradas > 1 ? "s" : ""
+    }: $${cantidadEntradas * precio2D}`;
   } else if (valorSeleccionado.includes("3D")) {
     precio.innerHTML = `$${precio3D}`;
-    if (cantidadEntradas == 1) {
-      total.innerHTML = `${cantidadEntradas} Entrada: $${
-        cantidadEntradas * precio3D
-      }`;
-    } else {
-      total.innerHTML = `${cantidadEntradas} Entradas: $${
-        cantidadEntradas * precio3D
-      }`;
-    }
+    total.innerHTML = `${cantidadEntradas} Entrada${
+      cantidadEntradas > 1 ? "s" : ""
+    }: $${cantidadEntradas * precio3D}`;
   }
 }
 
 function agregarACarrito() {
   const carrito = document.getElementById("carrito");
 
-  // Usar delegación de eventos para manejar los botones "Comprar Entradas"
   document.addEventListener("click", function (event) {
-    if (event.target && event.target.classList.contains("comprar-entradas")) {
+    if (event.target && event.target.classList.contains("btn")) {
       const tituloPelicula = event.target.dataset.titulo;
       const imgPelicula = event.target.dataset.img;
-      const offcanvas = new bootstrap.Offcanvas(
-        document.getElementById("offcanvasTop")
-      );
+
+      const offcanvasElement = document.getElementById("offcanvasRight");
+
+      // Verificar si el elemento existe
+      if (!offcanvasElement) {
+        console.error("El elemento offcanvas no se encontró en el DOM.");
+        return;
+      }
+
+      const offcanvas = new bootstrap.Offcanvas(offcanvasElement, {
+        backdrop: true, // Asegúrate de incluir la configuración para el backdrop
+      });
       offcanvas.show();
+
       carrito.innerHTML = `
-      <div>
-                        <img src="${imgPelicula}" alt="${tituloPelicula}">
-                     <div>
-                        <h3>${tituloPelicula}</h2>
-                        <select id="funciones" onchange="mostrarValorSeleccionado()">
-                        <option selected>Martes 9/07/2024 16:00hs 2D</option>
-                        <option>Martes 9/07/2024 21:00hs 3D SUB</option>
-                        <option>Miércoles 10/07/2024 18:30hs 2D</option>
-                        <option>Miércoles 10/07/2024 23:00hs 3D</option>
-                        <option>Jueves 11/07/2024 16:30hs 2D</option>
-                        <option>Jueves 11/07/2024 20:30hs 3D</option>
-                        <option>Viernes 12/07/2024 19:00hs 2D SUB</option>
-                        <option>Viernes 12/07/2024 21:30hs 3D</option>
-                        <option>Sábado 13/07/2024 17:30hs 2D</option>
-                        <option>Sábado 13/07/2024 22:30hs 3D SUB</option>
-                        <option>Domingo 14/07/2024 18:00hs 2D</option>
-                        <option>Domingo 14/07/2024 20:30hs 3D</option>
-                        </select>
-
-                        <div class="precio-carrito">
-                        <input type="number" min="1" max="5" placeholder="0" value="1" id="cantidad-entradas" oninput="mostrarValorSeleccionado()"></input>
-                        <p>X</p>                      
-                        <h4 id="precio"> </h4>
-                        </div>
-                        </div>
-                        </div>
-                        
-
-                        <div class="precio-final">
-                        <div>
-                        <h4>Total:</h4>
-                        <h4 id="precio-total" > </h4>
-                        </div>
-                        <button>Comprar</button>
-                        </div>
-                    `;
+        <div>
+          <img src="${imgPelicula}" alt="${tituloPelicula}">
+          <div>
+            <h3>${tituloPelicula}</h3>
+            <select id="funciones" onchange="mostrarValorSeleccionado()">
+              <option selected>Martes 9/07/2024 16:00hs 2D</option>
+              <option>Martes 9/07/2024 21:00hs 3D SUB</option>
+              <option>Miércoles 10/07/2024 18:30hs 2D</option>
+              <option>Miércoles 10/07/2024 23:00hs 3D</option>
+              <option>Jueves 11/07/2024 16:30hs 2D</option>
+              <option>Jueves 11/07/2024 20:30hs 3D</option>
+              <option>Viernes 12/07/2024 19:00hs 2D SUB</option>
+              <option>Viernes 12/07/2024 21:30hs 3D</option>
+              <option>Sábado 13/07/2024 17:30hs 2D</option>
+              <option>Sábado 13/07/2024 22:30hs 3D SUB</option>
+              <option>Domingo 14/07/2024 18:00hs 2D</option>
+              <option>Domingo 14/07/2024 20:30hs 3D</option>
+            </select>
+            <div class="precio-carrito">
+              <input type="number" min="1" max="5" placeholder="0" value="1" id="cantidad-entradas" oninput="mostrarValorSeleccionado()">
+              <p>X</p>
+              <h4 id="precio"></h4>
+            </div>
+          </div>
+        </div>
+        <div class="precio-final">
+          <div>
+            <h4>Total:</h4>
+            <h4 id="precio-total"></h4>
+          </div>
+          <button>Comprar</button>
+        </div>
+      `;
       mostrarValorSeleccionado();
     }
   });
--- scripts/peliculaBuscada.js
+++ scripts/peliculaBuscada.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const formPelicula = document.getElementById("form-pelicula");
   const inputBuscar = document.getElementById("buscar");
+  const posters = document.getElementById("posters");
+  const loading = document.getElementById("loading");
 
   formPelicula.addEventListener("submit", (evt) => {
     evt.preventDefault();
@@ -11,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function mostrarPeliculasBuscadas() {
       posters.innerHTML = "";
+      loading.style.display = "block";
       mostrarPeliculas(pelicula);
     }
 
@@ -25,35 +28,117 @@ document.addEventListener("DOMContentLoaded", () => {
         },
       };
 
-      let link = fetch(url, options);
-
-      link
+      fetch(url, options)
         .then((resultado) => resultado.json())
         .then((data) => {
           const movies = data.titleResults.results;
-          const title = [];
-          const img = [];
-          const credits = [];
-          movies.forEach((elemento) => title.push(elemento.titleNameText));
-          movies.forEach((elemento) =>
-            img.push(elemento.titlePosterImageModel.url)
-          );
-          movies.forEach((elemento) => credits.push(elemento.topCredits));
-          const posters = document.getElementById("posters");
-          for (let i = 0; i < title.length; i++) {
+          posters.innerHTML = ""; // Limpiar los posters anteriores
+
+          movies.forEach((elemento) => {
+            const title = elemento.titleNameText;
+            const img = elemento.titlePosterImageModel.url;
+            const credits = elemento.topCredits || "Información no disponible";
+
             const poster = `
-      <div>
-      <h2>${title[i]}</h2>
-      <img src="${img[i]}"/>
-      <p> Reparto: ${credits[i]}</p>
-      <button>Comprar Entradas</button>
-      </div>
-      `;
+              <div>
+                <h2>${title}</h2>
+                <img src="${img}" alt="${title}"/>
+                <p>Reparto: ${credits}</p>
+                <button class="comprar-entradas" data-titulo="${title}" data-img="${img}">Comprar Entradas</button>
+              </div>
+            `;
             posters.innerHTML += poster;
-          }
+          });
         })
-        .catch((error) => console.log("Error:", error));
+        .catch((error) => console.log("Error:", error))
+        .finally(() => {
+          loading.style.display = "none";
+        });
     }
     inputBuscar.value = "";
   });
+
+  function mostrarValorSeleccionado() {
+    const select = document.getElementById("funciones");
+    const valorSeleccionado = select.options[select.selectedIndex].text;
+    const precio = document.getElementById("precio");
+    const total = document.getElementById("precio-total");
+    const cantidadEntradas = document.getElementById("cantidad-entradas").value;
+    const precio2D = 9200;
+    const precio3D = 11200;
+
+    if (valorSeleccionado.includes("2D")) {
+      precio.innerHTML = `$${precio2D}`;
+      total.innerHTML = `${cantidadEntradas} Entrada${
+        cantidadEntradas > 1 ? "s" : ""
+      }: $${cantidadEntradas * precio2D}`;
+    } else if (valorSeleccionado.includes("3D")) {
+      precio.innerHTML = `$${precio3D}`;
+      total.innerHTML = `${cantidadEntradas} Entrada${
+        cantidadEntradas > 1 ? "s" : ""
+      }: $${cantidadEntradas * precio3D}`;
+    }
+  }
+
+  function agregarACarrito() {
+    const carrito = document.getElementById("carrito");
+
+    document.addEventListener("click", function (event) {
+      if (event.target && event.target.classList.contains("comprar-entradas")) {
+        const tituloPelicula = event.target.dataset.titulo;
+        const imgPelicula = event.target.dataset.img;
+
+        const offcanvasElement = document.getElementById("offcanvasRight");
+
+        // Verificar si el elemento existe
+        if (!offcanvasElement) {
+          console.error("El elemento offcanvas no se encontró en el DOM.");
+          return;
+        }
+
+        const offcanvas = new bootstrap.Offcanvas(offcanvasElement, {
+          backdrop: true, // Asegúrate de incluir la configuración para el backdrop
+        });
+        offcanvas.show();
+
+        carrito.innerHTML = `
+          <div>
+            <img src="${imgPelicula}" alt="${tituloPelicula}">
+            <div>
+              <h3>${tituloPelicula}</h3>
+              <select id="funciones" onchange="mostrarValorSeleccionado()">
+                <option selected>Martes 9/07/2024 16:00hs 2D</option>
+                <option>Martes 9/07/2024 21:00hs 3D SUB</option>
+                <option>Miércoles 10/07/2024 18:30hs 2D</option>
+                <option>Miércoles 10/07/2024 23:00hs 3D</option>
+                <option>Jueves 11/07/2024 16:30hs 2D</option>
+                <option>Jueves 11/07/2024 20:30hs 3D</option>
+                <option>Viernes 12/07/2024 19:00hs 2D SUB</option>
+                <option>Viernes 12/07/2024 21:30hs 3D</option>
+                <option>Sábado 13/07/2024 17:30hs 2D</option>
+                <option>Sábado 13/07/2024 22:30hs 3D SUB</option>
+                <option>Domingo 14/07/2024 18:00hs 2D</option>
+                <option>Domingo 14/07/2024 20:30hs 3D</option>
+              </select>
+              <div class="precio-carrito">
+                <input type="number" min="1" max="5" placeholder="0" value="1" id="cantidad-entradas" oninput="mostrarValorSeleccionado()">
+                <p>X</p>                      
+                <h4 id="precio"> </h4>
+              </div>
+            </div>
+          </div>
+          <div class="precio-final">
+            <div>
+              <h4>Total:</h4>
+              <h4 id="precio-total"></h4>
+            </div>
+            <button>Comprar</button>
+          </div>
+        `;
+        mostrarValorSeleccionado();
+      }
+    });
+  }
+
+  agregarACarrito();
 });
